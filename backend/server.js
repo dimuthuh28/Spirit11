1b015bd13acb6b22a90b4003a2eceaa493831273
dimuthuh28 <dimuthuharshamal@gmail.com>
2025-03-08 22:16:32 +0530

    Initial setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+// Initialize Express app
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// Connect Database
+connectDB();
+
+// Routes
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
